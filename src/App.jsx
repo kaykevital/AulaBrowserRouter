ede1601c163eb39bf3bb955c85f9d4dee253c969
kaykevital <kayke.macedo@estudante.ifms.edu.br>
2024-08-20 17:25:42 -0400

    edicao

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import Header from "./components/Header/Header";
+import { Link } from "react-router-dom";
+import "./globals.css";
 
 export default function App () {
     return(
-       <div>
-        <Header/>
-       </div>    
+        <div>
+            <ol type='1'>
+                <li>
+                    <Link to="/Home">Atividade 1: Criar componente de página Home</Link>
+                </li>
+            </ol> 
+        </div>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+     <BrowserRouter>
+        <Routes>
+        <Route element={<App/>} path='/'/>
+          <Route element={<PrimeiraAtividade/>} path='/Home'/>
+        </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
 
