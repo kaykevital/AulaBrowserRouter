1f0c28492ac0e8f81c31cd5af343f236b944926a
kayke <kayke.macedo@estudante.ifms.edu.br>
2024-08-21 13:53:45 -0300

    Adicao de arquivo e carrosel

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,7 +1,47 @@
 import Header from "../components/Header/Header";
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { useState } from "react";
+
 
 export default function Home () {
+    const [produtosHome, setProdutoshome] = useState([
+        {
+
+            id: 1,
+
+            item: "Hambúrguer",
+
+            imagem: "https://www.assai.com.br/sites/default/files/shutterstock_1806472312.jpg",
+
+            preco: "R$ 25,99"
+
+        },
+    ])
+
+
     return(
-        <Header/>
+       
+        <div className="principalHome">
+            
+            <Header/>
+            <div className="Home-Carrossel">
+                <Carousel infiniteLoop useKeyboardArrows autoPlay showArrows={true} showStatus={false} showThumbs={false} dynamicHeight>
+                <div>
+                        <img src="src/imgs/pernadrag2.png" alt="Slide 3" />
+                    </div>
+                </Carousel>
+            </div>
+            <div className="Home-Produtos">
+                {
+                    produtosHome.map((produto)=> 
+                        <div key={produto.id}>
+                                <img src={produto.imagem}/>
+                                <p> { produto.item} </p>
+                        </div>
+                    )
+                }
+            </div>
+        </div>
     )
-}
+}   
